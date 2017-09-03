c500f3d039481e6dca6ec63570591a7f42dd9e11
Ace <akhilark1088@gmail.com>
2017-09-02 23:59:33 -0400

    Setting up server.

--- server.js
+++ server.js
@@ -2,4 +2,19 @@
 var express = require('express');
 var app = express();
 
+// setting view engine
 app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+// setting port
+app.listen(8080);
+console.log('Listening at port 8080');
